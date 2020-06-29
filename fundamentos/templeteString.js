2e701644f687f4fe397e164f5b93755abbe50c90
Polixenia Correia <polixeniacorreia@gmail.com>
2020-06-29 13:09:07 -0300

    Templete String

---
+++ fundamentos/templeteString.js
@@ -0,0 +1,11 @@
+const nome = 'Poli'
+const concatenacao = 'olá' +nome+ '!'
+const templete = `
+    Olá
+    ${nome}!`
+    
+console.log(templete)
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
