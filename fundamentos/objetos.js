0eefdb8469db191d5ca4f8cbcf992ca53ff953c5
Polixenia Correia <polixeniacorreia@gmail.com>
2020-06-30 10:08:02 -0300

    Objeto

---
+++ fundamentos/objetos.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "Celular"
+prod1.preço = 3000
+prod1['Desconto'] = 0.50
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preço: 99.99,   
+}
+
+console.log(prod2)
