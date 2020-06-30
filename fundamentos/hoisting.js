b6c17b48e58fdaf763938a4c3ed4346f6d5bffa7
Polixenia Correia <polixeniacorreia@gmail.com>
2020-06-30 12:17:27 -0300

    Hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,12 @@
+function tete() {
+    console.log('a =', a)
+    var a = 6
+    console.log('a =', a)
+}
+
+tete()
+//console.log('a =', a)
+
+console.log('b =', b)
+var b = 203
+console.log('b =', b)
