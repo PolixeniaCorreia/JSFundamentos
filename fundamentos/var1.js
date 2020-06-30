cdaf208de2f3ea64c34540b88eafc49df5785fc1
Polixenia Correia <polixeniacorreia@gmail.com>
2020-06-30 11:39:07 -0300

    Exemplo Var1

---
+++ fundamentos/var1.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            var sera = 'Será????'
+            console.log(sera)
+        }
+    }
+}
+
+console.log(sera)
+
+function teste(){
+    var local = 156
+    console.log(local)
+}
+
+teste()
+//console.log(local)
