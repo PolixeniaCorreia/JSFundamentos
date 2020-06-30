c6f836d86e57bf5632629c48dca3a6b566d75797
Polixenia Correia <polixeniacorreia@gmail.com>
2020-06-30 12:33:00 -0300

    Chave Valor

---
+++ fundamentos/chaveValor.js
@@ -0,0 +1,19 @@
+const saudacao = 'Opa'
+
+function exec(){
+    const saudacao = 'falaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 59,
+    endereco:{
+        logradouro: 'Rua dos Prazeres',
+        numero: 89
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
